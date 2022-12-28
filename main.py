5a33200690ed0e1aa697418217ac4534ed5048d9
ouhoy <kimkinon@gmail.com>
2022-12-28 17:48:09 +0100

    Add simple functionality for the system

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import datetime
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-            'v', 'w', 'x', 'y', 'z', " "]
 customer_details = {
     "nights_spent": 0,
-    "ordered_food": []
+    "ordered_food": [],
+    "laundry": []
+
 }
 
 bill = {
@@ -33,8 +33,85 @@ food_menu = {
     6: {"item": "ItemSix", "price": 23},
 }
 
+laundry_types = {
+    1: {"item": "Normal laundry", "price": 2},
+    2: {"item": "Special laundry", "price": 4},
+    3: {"item": "Uniform laundry", "price": 10},
+}
+
+
+class DataValidation:
+    @staticmethod
+    def name_validation(prompt_string):
+
+        alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                    'u',
+                    'v', 'w', 'x', 'y', 'z', " "]
+        name = input(prompt_string).lower().strip()
+        if len(name) > 512:
+            print("name too long")
+            return name_validation(prompt_string)
+        if len(name) < 3:
+            print("name too short")
+            return name_validation(prompt_string)
+        for element in name:
+            if not element in alphabet:
+                print(element)
+                print("Make sure that your name does not contain any numbers or symbols.")
+                return name_validation(prompt_string)
+        return name
+
+    @staticmethod
+    def date_validation(prompt_string, date_type="date"):
+        date = input(prompt_string).strip().split("-")
+        for n in date:
+            if not n.isnumeric():
+                print(f"Please, make sure that the {date_type} does not contain any characters or spaces.")
+                return date_validation(prompt_string)
+        if len(date[2]) != 4:
+            print("Please put in the year in this format yyyy")
+            return date_validation(prompt_string)
+        day = int(date[0])
+        month = int(date[1])
+        year = int(date[2])
+        # If the used checked for an earlier date from today
+        if "check-in" in customer_details:
+            print("Working...")
+            dates = datetime.date(year, month, day) - datetime.date(checkin_date["year"], checkin_date["month"],
+                                                                    checkin_date["day"])
+            if 0 > dates.days:
+                print(f"Enter a valid {date_type} up to one day from today")
+                return date_validation(prompt_string)
+        try:
+            date_calc = datetime.date.today() - datetime.date(year, month, day)
+            if date_calc.days > 0:
+                print(datetime.date.today())
+                print(datetime.date(year, month, day))
+                print(f"Enter a valid {date_type} up to one day from today")
+                return date_validation(prompt_string)
+
+        except Exception as e:
+            print(e)
+            return date_validation(prompt_string)
+        return {"day": day, "month": month, "year": year}
+
+    @staticmethod
+    def num_input_validation(prompt_string, rng=range(0)):
+        num = input(prompt_string).strip()
+        if num.isnumeric():
+
+            if len(rng) > 0 and int(num) in rng:
+                return int(num)
+            elif not rng:
+                return int(num)
+
+        print("Please put a valid number")
+        return num_input_validation(prompt_string, rng)
+
 
 def name_validation(prompt_string):
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+                'v', 'w', 'x', 'y', 'z', " "]
     name = input(prompt_string).lower().strip()
     if len(name) > 512:
         print("name too long")
@@ -64,20 +141,21 @@ def date_validation(prompt_string, date_type="date"):
     year = int(date[2])
     # If the used checked for an earlier date from today
     if "check-in" in customer_details:
-        print("Working...")
-        dates = datetime.date(year, month, day) - datetime.date(checkin_date["year"], checkin_date["month"],
-                                                                checkin_date["day"])
-        if 0 > dates.days:
-            print(f"Enter a valid {date_type} up to one day from today")
+        try:
+
+            dates = datetime.date(year, month, day) - datetime.date(checkin_date["year"], checkin_date["month"],
+                                                                    checkin_date["day"])
+            if 0 > dates.days:
+                print(f"Enter a valid {date_type} up to one day from today")
+                return date_validation(prompt_string)
+        except Exception as e:
+            print(e)
             return date_validation(prompt_string)
     try:
         date_calc = datetime.date.today() - datetime.date(year, month, day)
         if date_calc.days > 0:
-            print(datetime.date.today())
-            print(datetime.date(year, month, day))
             print(f"Enter a valid {date_type} up to one day from today")
             return date_validation(prompt_string)
-
     except Exception as e:
         print(e)
         return date_validation(prompt_string)
@@ -109,8 +187,6 @@ def room_checkin():
         "In order to make your selection, please input a number between 1 and 4 corresponding to the "
         "desired room type from the list.",
         rng=range(1, 5))
-    # customer_details["chosen_room_type"] = chosen_room
-    # bill["room_rent"] += customer_details["nights_spent"] * room_price
 
     return room_types[chosen_item]
 
@@ -124,10 +200,20 @@ def get_food():
     return food_menu[chosen_item]
 
 
-user_name = name_validation("Write customer's name: ")
-customer_details["user_name"] = user_name
-user_address = input("Write the customer's address: ")
-customer_details["user_address"] = user_address
+def laundry():
+    print("We offer four types of laundry")
+    print_priced_items(laundry_types)
+    chosen_laundry_type = num_input_validation("Please enter the desired laundry type: ", range(1, 4))
+    weight = num_input_validation("Please enter the desired laundry weight nothing above 5kg: ", range(1, 5))
+    price = laundry_types[chosen_laundry_type]["price"] * weight
+    print(f"Chosen laundry type: {laundry_types[chosen_laundry_type]['item']} ")
+    print(f"Weight: {weight}kg ")
+    print(f"Total: ${price} ")
+    return {"type": laundry_types[chosen_laundry_type], "weight": weight, "price": price}
+
+
+customer_details["user_name"] = name_validation("Write customer's name: ")
+customer_details["user_address"] = input("Write the customer's address: ")
 
 # Get and save the check-in and check-out dates
 checkin_date = date_validation("Check-in date: ")
@@ -139,20 +225,35 @@ customer_details["nights_spent"] = (datetime.date(checkout_date["year"], checkou
                                                   checkout_date["day"]) - datetime.date(
     checkin_date["year"], checkin_date["month"], checkin_date["day"])).days
 
+##### Functional Area #####
+# Get room:
 get_room = room_checkin()
-
 customer_details["chosen_room"] = get_room
 customer_details["room_rent"] = customer_details["nights_spent"] * get_room["price"]
 bill["room_rent"] = customer_details["nights_spent"] * get_room["price"]
 
-calc_food = get_food()
+service = num_input_validation("Get served: ")
 
-customer_details["ordered_food"].append(calc_food)
+# Order food
+if service == 1:
+    ordered_food = get_food()
+    customer_details["ordered_food"].append(ordered_food)
+    bill["restaurant"] += ordered_food["price"]
 
-for food in customer_details["ordered_food"]:
-    bill["restaurant"] += food["price"]
+# Do laundry
+elif service == 2:
+    laundry_service = laundry()
+    customer_details["laundry"].append(laundry_service)
+    bill["laundry"] += laundry_service["price"]
 
 # Calc total
+
+# for food in customer_details["ordered_food"]:
+#     bill["restaurant"] += food["price"]
+#
+# for washed in customer_details["laundry"]:
+#     bill["laundry"] += washed["price"]
+
 for expense in bill:
     total += bill[expense]
 
@@ -162,15 +263,15 @@ def print_total_cost():
     print("**** Customer details ****")
     print("Customer Name: ", customer_details["user_name"])
     print("Customer Address: ", customer_details["user_address"])
-    print("Check-in Date: ", customer_details["check-in"])
+    print("Check-in Date: ", customer_details["check-in"]["day"], customer_details["check-in"]["month"],
+          customer_details["check-in"]["year"])
     print("Check-out Date: ", customer_details["check-out"]["day"], customer_details["check-out"]["month"],
           customer_details["check-out"]["year"])
-    print("Chosen Room Type: ", customer_details["chosen_room"]["day"], customer_details["chosen_room"]["month"],
-          customer_details["chosen_room"]["year"])
+    print("Chosen Room Type: ", customer_details["chosen_room"])
     print("Number of nights spent: ", customer_details["nights_spent"])
     # Customer's Bill
     print("**** Customer's Bill ****")
-    # print("*** Customer's Bill ***")
+
     # room
     print("Room rent: ", bill["room_rent"])
     # restaurant
