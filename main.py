289db14acacd5d79eef652aac62e228df5e33aa9
ouhoy <kimkinon@gmail.com>
2022-12-31 01:54:18 +0100

    Apply KISS principle

--- main.py
+++ main.py
@@ -18,100 +18,23 @@ bill = {
 
 total = 0
 
-room_types = {
-    1: {"item": "roomTypeOne", "price": 8},
-    2: {"item": "roomTypeTwo", "price": 15},
-    3: {"item": "roomTypeThree", "price": 24},
-    4: {"item": "roomTypeFour", "price": 35},
-}
-food_menu = {
-    1: {"item": "ItemOne", "price": 12},
-    2: {"item": "ItemTwo", "price": 82},
-    3: {"item": "ItemThree", "price": 15},
-    4: {"item": "ItemFour", "price": 18},
-    5: {"item": "ItemFive", "price": 35},
-    6: {"item": "ItemSix", "price": 23},
-}
-
-laundry_types = {
-    1: {"item": "Normal laundry", "price": 2},
-    2: {"item": "Special laundry", "price": 4},
-    3: {"item": "Uniform laundry", "price": 10},
-}
-
-
-class DataValidation:
-    @staticmethod
-    def name_validation(prompt_string):
-
-        alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-                    'u',
-                    'v', 'w', 'x', 'y', 'z', " "]
-        name = input(prompt_string).lower().strip()
-        if len(name) > 512:
-            print("name too long")
-            return name_validation(prompt_string)
-        if len(name) < 3:
-            print("name too short")
-            return name_validation(prompt_string)
-        for element in name:
-            if not element in alphabet:
-                print(element)
-                print("Make sure that your name does not contain any numbers or symbols.")
-                return name_validation(prompt_string)
-        return name
-
-    @staticmethod
-    def date_validation(prompt_string, date_type="date"):
-        date = input(prompt_string).strip().split("-")
-        for n in date:
-            if not n.isnumeric():
-                print(f"Please, make sure that the {date_type} does not contain any characters or spaces.")
-                return date_validation(prompt_string)
-        if len(date[2]) != 4:
-            print("Please put in the year in this format yyyy")
-            return date_validation(prompt_string)
-        day = int(date[0])
-        month = int(date[1])
-        year = int(date[2])
-        # If the used checked for an earlier date from today
-        if "check-in" in customer_details:
-            print("Working...")
-            dates = datetime.date(year, month, day) - datetime.date(checkin_date["year"], checkin_date["month"],
-                                                                    checkin_date["day"])
-            if 0 > dates.days:
-                print(f"Enter a valid {date_type} up to one day from today")
-                return date_validation(prompt_string)
-        try:
-            date_calc = datetime.date.today() - datetime.date(year, month, day)
-            if date_calc.days > 0:
-                print(datetime.date.today())
-                print(datetime.date(year, month, day))
-                print(f"Enter a valid {date_type} up to one day from today")
-                return date_validation(prompt_string)
-
-        except Exception as e:
-            print(e)
-            return date_validation(prompt_string)
-        return {"day": day, "month": month, "year": year}
-
-    @staticmethod
-    def num_input_validation(prompt_string, rng=range(0)):
-        num = input(prompt_string).strip()
-        if num.isnumeric():
-
-            if len(rng) > 0 and int(num) in rng:
-                return int(num)
-            elif not rng:
-                return int(num)
-
-        print("Please put a valid number")
-        return num_input_validation(prompt_string, rng)
+# TODO: 3) fix lang here
+room_types = [{"item": "roomTypeOne", "price": 8},
+              {"item": "roomTypeTwo", "price": 15},
+              {"item": "roomTypeThree", "price": 24},
+              {"item": "roomTypeFour", "price": 35}, ]
+food_menu = [{"item": "ItemOne", "price": 12},
+             {"item": "ItemTwo", "price": 82},
+             {"item": "ItemThree", "price": 15},
+             {"item": "ItemFour", "price": 18},
+             {"item": "ItemFive", "price": 35},
+             {"item": "ItemSix", "price": 23}, ]
+laundry_types = [{"item": "Normal laundry", "price": 2},
+                 {"item": "Special laundry", "price": 4},
+                 {"item": "Uniform laundry", "price": 10}, ]
 
 
 def name_validation(prompt_string):
-    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-                'v', 'w', 'x', 'y', 'z', " "]
     name = input(prompt_string).lower().strip()
     if len(name) > 512:
         print("name too long")
@@ -119,11 +42,9 @@ def name_validation(prompt_string):
     if len(name) < 3:
         print("name too short")
         return name_validation(prompt_string)
-    for element in name:
-        if not element in alphabet:
-            print(element)
-            print("Make sure that your name does not contain any numbers or symbols.")
-            return name_validation(prompt_string)
+    if not name.replace(" ", "").isalpha():
+        print("Make sure that your name does not contain any numbers or symbols.")
+        return name_validation(prompt_string)
     return name
 
 
@@ -176,8 +97,8 @@ def num_input_validation(prompt_string, rng=range(0)):
 
 
 def print_priced_items(ls):
-    for key, value in ls.items():
-        print(f'{key}) {value["item"]} - ${value["price"]}')
+    for item in range(len(ls)):
+        print(f'{item + 1}) {ls[item]["item"]} - ${ls[item]["price"]}')
 
 
 def room_checkin():
@@ -186,9 +107,9 @@ def room_checkin():
     chosen_item = num_input_validation(
         "In order to make your selection, please input a number between 1 and 4 corresponding to the "
         "desired room type from the list.",
-        rng=range(1, 5))
-
-    return room_types[chosen_item]
+        rng=range(1, len(room_types) + 1))
+    # TODO: 2) Don't forget the room service as an additional charge
+    return room_types[chosen_item - 1]
 
 
 def get_food():
@@ -196,20 +117,28 @@ def get_food():
     print_priced_items(food_menu)
     chosen_item = num_input_validation(
         "Kindly make your selection by inputting a number between 1 and 6 from the food menu options provided: ",
-        rng=range(1, 7))
-    return food_menu[chosen_item]
+        rng=range(1, len(food_menu) + 1))
+    return food_menu[chosen_item - 1]
+    # TODO: 1)  Don't forget the tep as an additional charge
 
 
 def laundry():
     print("We offer four types of laundry")
     print_priced_items(laundry_types)
-    chosen_laundry_type = num_input_validation("Please enter the desired laundry type: ", range(1, 4))
-    weight = num_input_validation("Please enter the desired laundry weight nothing above 5kg: ", range(1, 5))
-    price = laundry_types[chosen_laundry_type]["price"] * weight
+    chosen_laundry_type = num_input_validation("Please enter the desired laundry type: ",
+                                               range(1, len(laundry_types) + 1))
+    quantity = num_input_validation("Please enter the desired laundry quantity: ")
+    price = laundry_types[chosen_laundry_type]["price"] * quantity
+
     print(f"Chosen laundry type: {laundry_types[chosen_laundry_type]['item']} ")
-    print(f"Weight: {weight}kg ")
+    print(f"quantity: {quantity} ")
     print(f"Total: ${price} ")
-    return {"type": laundry_types[chosen_laundry_type], "weight": weight, "price": price}
+
+    return {"type": laundry_types[chosen_laundry_type], "quantity": quantity, "price": price}
+
+
+def game():
+    pass
 
 
 customer_details["user_name"] = name_validation("Write customer's name: ")
@@ -232,19 +161,25 @@ customer_details["chosen_room"] = get_room
 customer_details["room_rent"] = customer_details["nights_spent"] * get_room["price"]
 bill["room_rent"] = customer_details["nights_spent"] * get_room["price"]
 
-service = num_input_validation("Get served: ")
+while True:
+    user_functions = ["Order food", "Do laundry", "Print the bill"]
+    for i in range(len(user_functions)):
+        print(f"{i + 1}) {user_functions[i]} ")
+    service = num_input_validation("Get served: ", range(1, len(user_functions) + 1))
 
-# Order food
-if service == 1:
-    ordered_food = get_food()
-    customer_details["ordered_food"].append(ordered_food)
-    bill["restaurant"] += ordered_food["price"]
+    # Order food
+    if service == 1:
+        ordered_food = get_food()
+        customer_details["ordered_food"].append(ordered_food)
+        bill["restaurant"] += ordered_food["price"]
 
-# Do laundry
-elif service == 2:
-    laundry_service = laundry()
-    customer_details["laundry"].append(laundry_service)
-    bill["laundry"] += laundry_service["price"]
+    # Do laundry
+    if service == 2:
+        laundry_service = laundry()
+        customer_details["laundry"].append(laundry_service)
+        bill["laundry"] += laundry_service["price"]
+    if service == 3:
+        break
 
 # Calc total
 
@@ -275,10 +210,17 @@ def print_total_cost():
     # room
     print("Room rent: ", bill["room_rent"])
     # restaurant
-    print("Order name and price: ")
+    print("Restaurant bill: ")
     for order in customer_details["ordered_food"]:
         print(f"-{order['item']}, {order['price']}. ")
     print("Restaurant total: ", bill["restaurant"])
+
+    # laundry
+    print("Laundry bill: ")
+    for item in customer_details["laundry"]:
+        print(item)
+        # print(f"-{item['item']}, {item['price']}. ")
+    print("Laundry total: ", bill["laundry"])
     # Total
     print(f"Your total is: ", total)
 
